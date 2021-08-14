60bf56bb31f1ee00c6606e758d6b0919b528b9ea
harshit.jadon <harshit.jadon@MD-CJDKE06555KH.magicsw.com>
2021-08-14 23:05:35 +0530

    {Harshit} CREATE Footer of Bookstore HomePage

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'C:/Users/harshit.jadon.MD-CJDKE06555KH/Desktop/Bookstore/book-store/src/components/footer/Footer.scss';
+
+export default function footer() {
+    return (
+       
+            <footer className='footer-content'>
+                <div className='footer-div'>
+                    <p> Copyright &copy; 2020. Bookstore Private Limited. All Rights Reserved</p>
+                </div>
+               
+            </footer>
+    
+    )
+}
